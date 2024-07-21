1ecea3801e6824522e3c9b929b149cc27ecc1d39
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2024-07-21 21:29:40 +0530

    updated with session 20 git

--- Html & CSS/session 20/git/git.js
+++ Html & CSS/session 20/git/git.js
@@ -4,6 +4,10 @@
 // install it
 // use it
 
+// first time configuration
+// git config --global user.name "Your Name"
+// git config --global user.email "Your Email"
+// git config --global core.editor "code --wait"
 
 // git status
 // git init
@@ -12,3 +16,11 @@
 // git remote add origin 
 // git push -u origin master or git push origin master or git push -f origin master
 // git pull origin master
+
+// next time configuration
+// git status
+// git add . or git add filename
+// git commit -m "message"
+// git push origin master
+// git pull origin master
+
