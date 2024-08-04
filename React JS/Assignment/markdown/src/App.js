f18c841df20b8cd8e4affe8cef63c5ac3836eb20
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2024-08-04 20:22:55 +0530

    updateed with usestate assignment markdown 1

--- React JS/Assignment/markdown/src/App.js
+++ React JS/Assignment/markdown/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import Markdown from './Components/Markdown'
-import './App.css'
+import React, { useState } from 'react';
 
-const App = () => {
+
+import './App.css';
+import { marked } from 'marked';
+
+function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
-    <div>
-        <Markdown />
+    <div className="app">
+      <textarea
+        className="editor"
+        value={markdown}
+        onChange={handleChange}
+        placeholder="Write your markdown here..."
+      />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      />
+      
+       
     </div>
-  )
+    
+  );
 }
 
-export default App
+export default App;
